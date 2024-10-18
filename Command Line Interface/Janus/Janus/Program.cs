2759b516c2bb606434a8a19611118915104e6b2b
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-10-18 12:31:41 +0100

    Implemented empty & help command

--- Command Line Interface/Janus/Janus/Program.cs
+++ Command Line Interface/Janus/Janus/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("setup");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter your command...");
+                Console.WriteLine("Use \"janus help\" to get more details");
+                return;
+            }
+
+            // Check for "help"(ignoring cases)
+            if (string.Equals(args[0], "help", StringComparison.OrdinalIgnoreCase))
+            {
+                ShowHelp();
+                return;
+            }
+
         }
+
+
+        /*
+         * Displays the help method
+         */
+        static void ShowHelp()
+        {
+            Console.WriteLine("Usage: janus [command]");
+            Console.WriteLine("Commands:");
+            Console.WriteLine("  init       Initialize a repository");
+            Console.WriteLine("  commit     Commit changes");
+            Console.WriteLine("  push       Push to a remote repository");
+        }
+
     }
 }
