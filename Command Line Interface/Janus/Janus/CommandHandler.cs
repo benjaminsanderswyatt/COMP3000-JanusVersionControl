281c6667a96e0ba7788670d2a91c67df2255f783
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-10-18 12:53:58 +0100

    Init command initialises .janus folder

--- Command Line Interface/Janus/Janus/CommandHandler.cs
+++ Command Line Interface/Janus/Janus/CommandHandler.cs
@@ -6,13 +6,40 @@ using System.Threading.Tasks;
 
 namespace Janus
 {
-    internal class CommandHandler
+    public static class CommandHandler
     {
+        private static string janusDir = ".janus";
+
         public static void Init(string[] args)
         {
-            Console.WriteLine("Init");
+            // Initialise .janus folder
+            if (!Directory.Exists(janusDir))
+            {
+                Directory.CreateDirectory(janusDir);
+                File.SetAttributes(janusDir, File.GetAttributes(janusDir) | FileAttributes.Hidden); // Makes the janus folder hidden
+
+                Console.WriteLine("Initialized janus repository");
+            }
+            else
+            {
+                Console.WriteLine("Repository already initialized");
+            }
         }
 
+        public static void Add(string[] args)
+        {
+            Console.WriteLine("Add");
+        }
+
+        public static void Commit(string[] args)
+        {
+            Console.WriteLine("Commit");
+        }
+
+        public static void Log(string[] args)
+        {
+            Console.WriteLine("Log");
+        }
 
     }
 }
