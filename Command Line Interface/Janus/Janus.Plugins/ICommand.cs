61a6e155639ad6a58954075038abf358d62580d3
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-10-26 18:25:49 +0100

    Created shared interface for plugin

---
+++ Command Line Interface/Janus/Janus.Plugins/ICommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Janus.Plugins
+{
+    public interface ICommand
+    {
+        string Name { get; } // Command name
+        string Description { get; } // Command description
+        void Execute(string[] args); // Command execution logic
+
+    }
+}
