fd00cc6196d78c4236a8db654a17d7e678db7c05
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-10-26 18:26:55 +0100

    Created a demo plugin

---
+++ Command Line Interface/PluginDemo/PluginDemo/PluginDemo.cs
@@ -0,0 +1,15 @@
+﻿using Janus.Plugins;
+
+namespace PluginDemo
+{
+    public class PluginDemo : ICommand
+    {
+        public string Name => "plugin";
+        public string Description => "A demo command for plugins";
+
+        public void Execute(string[] args)
+        {
+            Console.WriteLine("Plugin Demo - hello world");
+        }
+    }
+}
