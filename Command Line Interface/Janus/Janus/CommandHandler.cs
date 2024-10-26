e0ba5a2e617f21c1975be7a6c40393b88cb4bd02
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-10-26 18:28:40 +0100

    Reworked built in commands to use ICommand

--- Command Line Interface/Janus/Janus/CommandHandler.cs
+++ Command Line Interface/Janus/Janus/CommandHandler.cs
@@ -1,153 +1,198 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Janus.Plugins;
 
 namespace Janus
 {
     public static class CommandHandler
     {
-        public static void Init(string[] args)
+        public static List<ICommand> GetCommands()
         {
-            // Initialise .janus folder
-            if (!Directory.Exists(Paths.janusDir))
+            return new List<ICommand>
             {
-                Directory.CreateDirectory(Paths.janusDir);
-            } else
-            {
-                Console.WriteLine("Repository already initialized");
-                return;
-            }
+                new InitCommand(),
+                new AddCommand(),
+                new CommitCommand(),
+                new CreateBranchCommand(),
+                new SwitchBranchCommand(),
+                new LogCommand()
 
-            File.SetAttributes(Paths.janusDir, File.GetAttributes(Paths.janusDir) | FileAttributes.Hidden); // Makes the janus folder hidden
-
-            // .janus/object folder
-            if (!Directory.Exists(Paths.objectDir))
-                Directory.CreateDirectory(Paths.objectDir);
-
-            // .janus/refs
-            if (!Directory.Exists(Paths.refsDir))
-                Directory.CreateDirectory(Paths.refsDir);
-
-            // .janus/refs/heads
-            if (!Directory.Exists(Paths.headsDir))
-                Directory.CreateDirectory(Paths.headsDir);
-
-
-            // Create index file
-            File.Create(Paths.index).Close();
-
-            // Create empty main branch in refs/heads/
-            File.WriteAllText(Path.Combine(Paths.headsDir, "main"), string.Empty);
-
-            // Create HEAD file pointing at main branch
-            File.WriteAllText(Paths.head, "ref: refs/heads/main");
-
-            Console.WriteLine("Initialized janus repository");
-   
+                // Add new built in commands here
+            };
         }
 
-        public static void Add(string[] args)
+
+        public class InitCommand : ICommand
         {
-            string fileName = args[0];
-
-            string blobHash = CommandHelper.SaveBlob(fileName);
-
-            // Add the file and its hash to the staged area
-            File.AppendAllText(Paths.index, $"{fileName} {blobHash}\n");
-
-
-            Console.WriteLine($"Added {fileName} (blob {blobHash}).");
-        }
-
-        public static void Commit(string[] args)
-        {
-            try
+            public string Name => "init";
+            public string Description => "Initializes the repository.";
+            public void Execute(string[] args)
             {
-                string message = args[0];
-
-                // Get files from staging area
-                var files = new Dictionary<string, string>();
-
-                foreach (var line in File.ReadLines(Paths.index))
+                // Initialise .janus folder
+                if (!Directory.Exists(Paths.janusDir))
                 {
-                    var parts = line.Split(' ');
-                    files[parts[0]] = parts[1];
+                    Directory.CreateDirectory(Paths.janusDir);
+                }
+                else
+                {
+                    Console.WriteLine("Repository already initialized");
+                    return;
                 }
 
-                string treeHash = CommandHelper.SaveTree(files);
-                string commitHash = CommandHelper.SaveCommit(treeHash, message);
+                File.SetAttributes(Paths.janusDir, File.GetAttributes(Paths.janusDir) | FileAttributes.Hidden); // Makes the janus folder hidden
 
-                // Clear staging area (index)
-                File.WriteAllText(Paths.index, string.Empty);
+                // .janus/object folder
+                if (!Directory.Exists(Paths.objectDir))
+                    Directory.CreateDirectory(Paths.objectDir);
 
-                // Write current commit hash to refs/heads/[current branch]
-                string currentBranch = File.ReadAllText(Paths.head).Replace("ref: ", "").Trim();
-                string branchPath = Path.Combine(Paths.janusDir, currentBranch);
-                File.WriteAllText(branchPath, commitHash);
+                // .janus/refs
+                if (!Directory.Exists(Paths.refsDir))
+                    Directory.CreateDirectory(Paths.refsDir);
 
-                Console.WriteLine($"Committed to {currentBranch} with hash {commitHash}");
+                // .janus/refs/heads
+                if (!Directory.Exists(Paths.headsDir))
+                    Directory.CreateDirectory(Paths.headsDir);
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-
-
-           }
-
-        public static void CreateBranch(string[] args)
-        {
-            string branchName = args[0];
-            string branchPath = Path.Combine(Paths.headsDir, branchName);
-
-            if (File.Exists(branchPath))
-            {
-                Console.WriteLine($"Branch '{branchName}' already exists.");
-                return;
-            }
-
-            // Get the latest commit of the curent branch
-            string headPath = Path.Combine(Paths.janusDir, "HEAD");
-            string currentBranch = File.ReadAllText(headPath).Replace("ref: ", "").Trim();
-            string currentBranchPath = Path.Combine(Paths.janusDir, currentBranch);
-
-            // Write the latest commit hash into the new branch file
-            string latestCommitHash = File.ReadAllText(currentBranchPath).Trim();
-            File.WriteAllText(branchPath, latestCommitHash);
+                // .janus/plugins folder
+                if (!Directory.Exists(Paths.pluginsDir))
+                    Directory.CreateDirectory(Paths.pluginsDir);
 
 
-            Console.WriteLine($"Created new branch {branchName}");
-        }
+                // Create index file
+                File.Create(Paths.index).Close();
 
-        public static void SwitchBranch(string[] args)
-        {
-            string branchName = args[0];
-            string branchPath = Path.Combine(Paths.headsDir, branchName);
+                // Create empty main branch in refs/heads/
+                File.WriteAllText(Path.Combine(Paths.headsDir, "main"), string.Empty);
 
-            if (File.Exists(branchPath))
-            {
-                // Update HEAD to point to the switched to branch
-                File.WriteAllText(Path.Combine(Paths.janusDir, "HEAD"), $"ref: refs/heads/{branchName}");
-                Console.WriteLine($"Switched to branch {branchName}.");
-            }
-            else
-            {
-                Console.WriteLine($"Branch {branchName} does not exist.");
+                // Create HEAD file pointing at main branch
+                File.WriteAllText(Paths.head, "ref: refs/heads/main");
+
+                Console.WriteLine("Initialized janus repository");
             }
         }
 
-
-        public static void Log(string[] args)
+        public class AddCommand : ICommand
         {
-            foreach (var commitFile in Directory.GetFiles(Paths.objectDir))
+            public string Name => "add";
+            public string Description => "add help";
+            public void Execute(string[] args)
             {
-                string content = File.ReadAllText(commitFile);
-                if (content.StartsWith("Tree:"))
+                string fileName = args[0];
+
+                string blobHash = CommandHelper.SaveBlob(fileName);
+
+                // Add the file and its hash to the staged area
+                File.AppendAllText(Paths.index, $"{fileName} {blobHash}\n");
+
+
+                Console.WriteLine($"Added {fileName} (blob {blobHash}).");
+            }
+        }
+
+        public class CommitCommand : ICommand
+        {
+            public string Name => "commit";
+            public string Description => "commit";
+            public void Execute(string[] args)
+            {
+                try
                 {
-                    Console.WriteLine(content);
+                    string message = args[0];
+
+                    // Get files from staging area
+                    var files = new Dictionary<string, string>();
+
+                    foreach (var line in File.ReadLines(Paths.index))
+                    {
+                        var parts = line.Split(' ');
+                        files[parts[0]] = parts[1];
+                    }
+
+                    string treeHash = CommandHelper.SaveTree(files);
+                    string commitHash = CommandHelper.SaveCommit(treeHash, message);
+
+                    // Clear staging area (index)
+                    File.WriteAllText(Paths.index, string.Empty);
+
+                    // Write current commit hash to refs/heads/[current branch]
+                    string currentBranch = File.ReadAllText(Paths.head).Replace("ref: ", "").Trim();
+                    string branchPath = Path.Combine(Paths.janusDir, currentBranch);
+                    File.WriteAllText(branchPath, commitHash);
+
+                    Console.WriteLine($"Committed to {currentBranch} with hash {commitHash}");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+
+            }
+        }
+
+        public class CreateBranchCommand : ICommand
+        {
+            public string Name => "create branch";
+            public string Description => "create branch help";
+            public void Execute(string[] args)
+            {
+                string branchName = args[0];
+                string branchPath = Path.Combine(Paths.headsDir, branchName);
+
+                if (File.Exists(branchPath))
+                {
+                    Console.WriteLine($"Branch '{branchName}' already exists.");
+                    return;
+                }
+
+                // Get the latest commit of the curent branch
+                string headPath = Path.Combine(Paths.janusDir, "HEAD");
+                string currentBranch = File.ReadAllText(headPath).Replace("ref: ", "").Trim();
+                string currentBranchPath = Path.Combine(Paths.janusDir, currentBranch);
+
+                // Write the latest commit hash into the new branch file
+                string latestCommitHash = File.ReadAllText(currentBranchPath).Trim();
+                File.WriteAllText(branchPath, latestCommitHash);
+
+
+                Console.WriteLine($"Created new branch {branchName}");
+            }
+        }
+
+        public class SwitchBranchCommand : ICommand
+        {
+            public string Name => "switch branch";
+            public string Description => "switch branch help";
+            public void Execute(string[] args)
+            {
+                string branchName = args[0];
+                string branchPath = Path.Combine(Paths.headsDir, branchName);
+
+                if (File.Exists(branchPath))
+                {
+                    // Update HEAD to point to the switched to branch
+                    File.WriteAllText(Path.Combine(Paths.janusDir, "HEAD"), $"ref: refs/heads/{branchName}");
+                    Console.WriteLine($"Switched to branch {branchName}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Branch {branchName} does not exist.");
+                }
+            }
+        }
+
+        public class LogCommand : ICommand
+        {
+            public string Name => "log";
+            public string Description => "log help";
+            public void Execute(string[] args)
+            {
+                foreach (var commitFile in Directory.GetFiles(Paths.objectDir))
+                {
+                    string content = File.ReadAllText(commitFile);
+                    if (content.StartsWith("Tree:"))
+                    {
+                        Console.WriteLine(content);
+                    }
                 }
             }
         }
--- Command Line Interface/Janus/Janus/Paths.cs
+++ Command Line Interface/Janus/Janus/Paths.cs
@@ -6,6 +6,7 @@
         public static readonly string objectDir = Path.Combine(janusDir, "objects");
         public static readonly string refsDir = Path.Combine(janusDir, "refs");
         public static readonly string headsDir = Path.Combine(refsDir, "heads");
+        public static readonly string pluginsDir = ".plugins";
 
         public static readonly string index = Path.Combine(janusDir, "index");
         public static readonly string head = Path.Combine(janusDir, "HEAD");
