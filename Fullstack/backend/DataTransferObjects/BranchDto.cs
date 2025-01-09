2d590a3dc1693437057254518aecfb9f96e00610
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-01-09 18:54:09 +0000

    Repo & Branch Dtos

---
+++ Fullstack/backend/DataTransferObjects/BranchDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.DataTransferObjects
+{
+    public class BranchDto
+    {
+        [Required]
+        public int RepoId { get; set; }
+
+        [Required]
+        [StringLength(256, ErrorMessage = "Branch name cannot exceed 256 characters.")]
+        public string BranchName { get; set; }
+
+        public int LatestCommitId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
---
+++ Fullstack/backend/DataTransferObjects/RepoDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.DataTransferObjects
+{
+    public class RepositoryDto
+    {
+        [Required]
+        [StringLength(256, ErrorMessage = "Repository name cannot exceed 256 characters.")]
+        public string RepoName { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
