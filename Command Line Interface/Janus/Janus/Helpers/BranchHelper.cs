ffeb99fb06edbc1664024b73f235ed4313a541cb
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-01-18 13:52:14 +0000

    Implemented rollback for failed branch deletion

--- Command Line Interface/Janus/Janus/Helpers/BranchHelper.cs
+++ Command Line Interface/Janus/Janus/Helpers/BranchHelper.cs
@@ -182,8 +182,12 @@ namespace Janus.Helpers
 
         public static void DeleteBranchCommitAndFiles(ILogger logger, Paths paths, string branchName)
         {
+            string tempDeletionDir = Path.Combine(paths.JanusDir, ".temp_deletion");
+
             try
             {
+                Directory.CreateDirectory(tempDeletionDir);
+
                 // Get all the commits in the commit directory
                 var allCommits = GetAllCommits(logger, paths);
 
@@ -192,16 +196,22 @@ namespace Janus.Helpers
 
 
                 // Find the files exclusive to branch
-                var branchFiles = branchCommits.SelectMany(c => c.Files.Keys).Distinct().ToHashSet();
+                var branchFiles = branchCommits.SelectMany(c => c.Files.Keys).ToHashSet();
 
                 var otherBranchFiles = allCommits.Where(c => c.Branch != branchName)
                                                  .SelectMany(c => c.Files.Keys)
-                                                 .Distinct()
                                                  .ToHashSet();
 
                 // Files exclusive to the branch
                 var filesToDelete = branchFiles.Except(otherBranchFiles).ToList();
 
+
+                // Backup the files to be deleted
+                BackupDeletion(paths, tempDeletionDir, filesToDelete, branchCommits);
+                
+
+
+                // Delete the files that are unique to the branch
                 foreach (var file in filesToDelete)
                 {
                     string objectFilePath = Path.Combine(paths.ObjectDir, file);
@@ -221,12 +231,88 @@ namespace Janus.Helpers
                     }
                 }
 
+                logger.Log($"Cleaned up files and commits from deleted branch.");
             }
             catch (Exception ex)
             {
                 logger.Log($"Failed to delete branch {branchName}: {ex.Message}");
+
+                RestoreBackupDeletion(paths, tempDeletionDir);
+
+            }
+
+
+            // Cleanup the temp deletion directory
+            if (Directory.Exists(tempDeletionDir))
+            {
+                Directory.Delete(tempDeletionDir, true);
+            }
+
+        }
+
+        private static void BackupDeletion(Paths paths, string tempDeletionDir, List<string>? filesToDelete, List<CommitMetadata>? branchCommits)
+        {
+            // Backup the files to be deleted into the temp directory
+            foreach (var file in filesToDelete)
+            {
+                string objectFilePath = Path.Combine(paths.ObjectDir, file);
+
+                if (File.Exists(objectFilePath))
+                {
+                    string destPath = Path.Combine(tempDeletionDir, "objects", file);
+
+                    Directory.CreateDirectory(Path.GetDirectoryName(destPath));
+                    File.Move(objectFilePath, destPath);
+                }
+            }
+
+            // Backup the commits to be deleted into the temp directory
+            foreach (var commit in branchCommits)
+            {
+                string commitFilePath = Path.Combine(paths.CommitDir, commit.Commit);
+                if (File.Exists(commitFilePath))
+                {
+                    string destPath = Path.Combine(tempDeletionDir, "commits", commit.Commit);
+
+                    Directory.CreateDirectory(Path.GetDirectoryName(destPath));
+                    File.Move(commitFilePath, destPath);
+                }
             }
         }
+
+        private static void RestoreBackupDeletion(Paths paths, string tempDeletionDir)
+        {
+            // Restore files
+            string backupObjectsDir = Path.Combine(tempDeletionDir, "objects");
+
+            if (Directory.Exists(backupObjectsDir))
+            {
+                foreach (var filePath in Directory.GetFiles(backupObjectsDir, "*", SearchOption.AllDirectories))
+                {
+                    string relativePath = Path.GetRelativePath(backupObjectsDir, filePath);
+                    string originalPath = Path.Combine(paths.ObjectDir, relativePath);
+
+                    Directory.CreateDirectory(Path.GetDirectoryName(originalPath));
+                    File.Move(filePath, originalPath, true);
+                }
+            }
+
+            // Restore commits
+            string backupCommitsDir = Path.Combine(tempDeletionDir, "commits");
+            if (Directory.Exists(backupCommitsDir))
+            {
+                foreach (var filePath in Directory.GetFiles(backupCommitsDir, "*", SearchOption.AllDirectories))
+                {
+                    string relativePath = Path.GetRelativePath(backupCommitsDir, filePath);
+                    string originalPath = Path.Combine(paths.CommitDir, relativePath);
+
+                    Directory.CreateDirectory(Path.GetDirectoryName(originalPath));
+                    File.Move(filePath, originalPath, true);
+                }
+            }
+        }
+
+
 
         public static List<CommitMetadata> GetAllCommits(ILogger logger, Paths paths)
         {
