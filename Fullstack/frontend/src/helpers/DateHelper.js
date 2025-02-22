363be8f2699494781719932b02f93fcdd80fa587
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-02-22 22:42:58 +0000

    Error handling for date readability

--- Fullstack/frontend/src/helpers/DateHelper.js
+++ Fullstack/frontend/src/helpers/DateHelper.js
@@ -1,18 +1,38 @@
 export const formatDate = (dateString) => {
-    
+  try {
     const date = new Date(dateString);
+
+    if (isNaN(date.getTime())) {
+      throw new Error("Invalid date string");
+    }
 
     return new Intl.DateTimeFormat('en-GB', { 
       dateStyle: 'medium', 
       timeStyle: 'short' 
     }).format(date);
+
+  } catch (error) {
+    console.error("Error formatting date:", error);
+
+    return "";
+  }
 };
 
 export const formatOnlyDate = (dateString) => {
-    
-  const date = new Date(dateString);
+  try {
+    const date = new Date(dateString);
 
-  return new Intl.DateTimeFormat('en-GB', { 
-    dateStyle: 'medium'
-  }).format(date);
+    if (isNaN(date.getTime())) {
+      throw new Error("Invalid date string");
+    }
+
+    return new Intl.DateTimeFormat('en-GB', { 
+      dateStyle: 'medium'
+    }).format(date);
+
+  } catch (error) {
+    console.error("Error formatting date:", error);
+
+    return "";
+  }
 };
