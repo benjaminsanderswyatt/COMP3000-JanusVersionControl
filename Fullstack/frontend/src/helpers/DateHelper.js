bc59c3c24a27d7dd471a52a5086d1255bc9db528
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-02-22 18:50:54 +0000

    Implemented helper to make date readable

--- Fullstack/frontend/src/helpers/DateHelper.js
+++ Fullstack/frontend/src/helpers/DateHelper.js
@@ -7,3 +7,12 @@ export const formatDate = (dateString) => {
       timeStyle: 'short' 
     }).format(date);
 };
+
+export const formatOnlyDate = (dateString) => {
+    
+  const date = new Date(dateString);
+
+  return new Intl.DateTimeFormat('en-GB', { 
+    dateStyle: 'medium'
+  }).format(date);
+};
