203d9c3728bb0e513d820e3b54145031fdc62897
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-03-09 17:38:08 +0000

    Fixed issue browser returning error on PP fallback

--- Fullstack/frontend/src/components/images/ProfilePic.js
+++ Fullstack/frontend/src/components/images/ProfilePic.js
@@ -3,34 +3,20 @@ import { useAuth } from "../../contexts/AuthContext";
 
 import styles from "../../styles/components/images/ProfilePic.module.css";
 
+
 const ProfilePic = ({ userId, label, alt, innerClassName, handleClick }) => {
     const { profilePicRefresh } = useAuth();
-    const [imageSrc, setImageSrc] = useState(getImageUrl(userId, profilePicRefresh));
   
-    // Helper function to build the URL with a cache-busting parameter
-    function getImageUrl(userId, timestamp) {
-        return `/images/${userId}.png?time=${timestamp}`;
-    }
-
-    // Update imageSrc whenever the refreshTimestamp or userId changes
-    useEffect(() => {
-        setImageSrc(getImageUrl(userId, profilePicRefresh));
-    }, [userId, profilePicRefresh]);
-
-
-
-    const handleError = () => {
-        setImageSrc("/icons/account.svg"); // Default image
-      };
+    // Server (nginx) returns the default icons/account.svg if it doesnt exist
+    const imageUrl = `/images/${userId}.png?time=${profilePicRefresh}`;
 
     return (
-      <div title={label} className={`${styles.main} ${innerClassName}`} onClick={() => handleClick()}>
-        <img
-          src={imageSrc}
-          alt={alt}
-          onError={handleError} // Set to default if doesnt exist
-          className={styles.image}
-        />
+      <div
+        title={label}
+        className={`${styles.main} ${innerClassName}`}
+        onClick={handleClick}
+      >
+        <img src={imageUrl} alt={alt} className={styles.image} />
       </div>
     );
 };
