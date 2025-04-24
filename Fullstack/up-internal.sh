756349854dc3bcd0443300621a02614909688840
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-04-24 16:19:48 +0100

    Fixed bug with janus PP not being set

--- Fullstack/up-internal.sh
+++ Fullstack/up-internal.sh
@@ -5,4 +5,7 @@ mkdir -p ./data/images
 mkdir -p ./data/files
 mkdir -p ./data/trees
 
+# Copy 0.png into the images folder
+cp ./frontend/public/0.png ./data/images/0.png
+
 docker-compose -f docker-compose-internal.yml up -d
